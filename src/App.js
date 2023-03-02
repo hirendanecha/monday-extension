4f80c60f858661d605ef0b7fd35a45f7c63490b9
Hiren Danecha <hirendanecha@gmail.com>
2023-03-02 15:06:44 +0530

    Changes fix.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AddDeal from "./components/google/AddDeal";
+import AddDealNew from "./components/google/AddDealNew";
 import SaveContact from "./components/google/SaveContact";
+import SaveContactNew from "./components/google/SaveContactNew";
 import SaveGmailScreen from "./components/google/SaveGmailScreen";
 import Home from "./components/Home";
 import InstallPage from "./components/InstallPage";
@@ -13,7 +15,9 @@ const App = () => {
         <Route path="/" element={<InstallPage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/googleSaveContact" element={<SaveContact />} />
+        <Route path="/googleSaveContactNew" element={<SaveContactNew />} />
         <Route path="/googleAddDeal" element={<AddDeal />} />
+        <Route path="/googleAddDealNew" element={<AddDealNew />} />
         <Route path="/saveGmail" element={<SaveGmailScreen />} />
       </Routes>
     </div>
